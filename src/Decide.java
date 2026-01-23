2a40b8f6f30ec00ce9ca3364b963f16559354d4e
SofiaTilde <sofia.tilde@gmail.com>
2026-01-23 14:37:14 +0100

    Add parameter comments for all functions

--- src/Decide.java
+++ src/Decide.java
@@ -15,19 +15,42 @@ public class Decide {
         return a + b;
     }
 
-    // Helper function to calculate Euclidian distance between two points
+    /**
+     * Helper function to calculate Euclidian distance between two points
+     * 
+     * @param p1
+     *            Point 1
+     * @param p2
+     *            Point 2
+     * @return Euclidian distance between points 1 and 2
+     */
     private static double distSq(Point2D.Double p1, Point2D.Double p2) {
         double dx = p1.x - p2.x;
         double dy = p1.y - p2.y;
         return dx * dx + dy * dy;
     }
 
-    // Helper function to calculate area of a triangle by three coordinates,
+    /**
+     * Helper function to calculate area of a triangle by three coordinates
+     * 
+     * @param p1
+     * @param p2
+     * @param p3
+     * @return
+     */
     private static double triangleArea(Point2D.Double p1, Point2D.Double p2, Point2D.Double p3) {
         return 0.5 * Math.abs(p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y));
     }
 
     // Helper function to determine if three points can be contained in a circle of radius R, following Welzl MEC algo
+    /**
+     * 
+     * @param p1
+     * @param p2
+     * @param p3
+     * @param radius
+     * @return
+     */
     private static boolean minRadius(Point2D.Double p1, Point2D.Double p2, Point2D.Double p3, double radius) {
 
         // Calc the distance between two points and diam to check if the distance between two points is > the diameter if not they cannot fit
@@ -61,7 +84,15 @@ public class Decide {
         return circumradius <= radius;
     }
 
-    // LIC 0 checks if two points are separated by a distance bigger than length 1
+    /**
+     * LIC 0 checks if two points are separated by a distance bigger than length 1
+     * 
+     * @param points
+     *            the list of points
+     * @param length1
+     *            the distance to check
+     * @return whether criteria LIC 0 is true or false
+     */
     public static boolean lic0(Point2D.Double[] points, double length1) {
         // Conditions to check for at least two points, and non-negative
         if (points == null || points.length < 2)
@@ -79,8 +110,15 @@ public class Decide {
         return false;
     }
 
-    // LIC 1 checks if there exist one set of thee data points that cant be contained within or on a circle of radius1
-
+    /**
+     * LIC 1 checks if there exist one set of three data points that can't be contained within or on a circle of radius1
+     * 
+     * @param points
+     *            the list of points
+     * @param radius1
+     *            the radius for the circle
+     * @return whether criteria LIC 1 is true or false
+     */
     public static boolean lic1(Point2D.Double[] points, double radius1) {
         // Checks if three points are given and non negative int
         if (points == null || points.length < 3) {
@@ -99,79 +137,157 @@ public class Decide {
         return false;
     }
 
-    // LIC 2 checks if
+    /**
+     * LIC 2 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 2 is true or false
+     */
     public static boolean lic2(Point2D.Double[] points) {
         // todo
         return false;
     }
 
-    // LIC 3 checks if
+    /**
+     * LIC 3 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 3 is true or false
+     */
     public static boolean lic3(Point2D.Double[] points) {
         // todo
         return false;
     }
 
-    // LIC 4 checks if
+    /**
+     * LIC 4 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 4 is true or false
+     */
     public static boolean lic4(Point2D.Double[] points) {
         // todo
         return false;
     }
 
-    // LIC 5 checks if
+    /**
+     * LIC 5 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 5 is true or false
+     */
     public static boolean lic5(Point2D.Double[] points) {
         // todo
         return false;
     }
 
-    // LIC 6 checks if
+    /**
+     * LIC 6 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 6 is true or false
+     */
     public static boolean lic6(Point2D.Double[] points) {
         // todo
         return false;
     }
 
-    // LIC 7 checks if
+    /**
+     * LIC 7 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 7 is true or false
+     */
     public static boolean lic7(Point2D.Double[] points) {
         // todo
         return false;
     }
 
-    // LIC 8 checks if
+    /**
+     * LIC 8 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 8 is true or false
+     */
     public static boolean lic8(Point2D.Double[] points) {
         // todo
         return false;
     }
 
-    // LIC 9 checks if
+    /**
+     * LIC 9 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 9 is true or false
+     */
     public static boolean lic9(Point2D.Double[] points) {
         // todo
         return false;
     }
 
-    // LIC 10 checks if
+    /**
+     * LIC 10 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 10 is true or false
+     */
     public static boolean lic10(Point2D.Double[] points) {
         // todo
         return false;
     }
 
-    // LIC 11 checks if
+    /**
+     * LIC 11 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 11 is true or false
+     */
     public static boolean lic11(Point2D.Double[] points) {
         // todo
         return false;
     }
 
-    // LIC 12 checks if
+    /**
+     * LIC 12 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 12 is true or false
+     */
     public static boolean lic12(Point2D.Double[] points) {
         // todo
         return false;
     }
 
-    // LIC 13 checks if
+    /**
+     * LIC 13 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 13 is true or false
+     */
     public static boolean lic13(Point2D.Double[] points) {
         // todo
         return false;
     }
 
-    // LIC 14 checks if
+    /**
+     * LIC 14 checks if
+     * 
+     * @param points
+     *            the list of points
+     * @return whether criteria LIC 14 is true or false
+     */
     public static boolean lic14(Point2D.Double[] points) {
         // todo
         return false;
