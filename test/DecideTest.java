0bbafc6239167a138e01391800b1eefbdfdf4575
SofiaTilde <sofia.tilde@gmail.com>
2026-01-26 16:13:01 +0100

    Add template for parameterized test

--- test/DecideTest.java
+++ test/DecideTest.java
@@ -1,5 +1,8 @@
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class DecideTest {
 
@@ -9,5 +12,16 @@ public class DecideTest {
     @Test
     void addWorks() {
         assertEquals(5, Decide.add(2, 3));
+    }
+
+    /**
+     * 
+     * Parameterized test for the Decide add function
+     */
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource(textBlock = "0, 1, 1\n1, 2, 3\n49, 51, 100\n1, 100, 101")
+    void testDecideAddFunction(int first, int second, int expectedResult) {
+        assertEquals(expectedResult, Decide.add(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
     }
 }
